b9b1f67c2b2f5413bdbc2a6475e94e396cb6bbc9
Alex <slayer1649@gmail.com>
2025-09-07 10:18:08 +0300

    added routes
    added routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PollController;
+use App\Http\Controllers\VoteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PollController::class, 'index'])->name('polls.index');
+
+Route::get('/polls/create', [PollController::class, 'create'])->name('polls.create');
+Route::post('/polls', [PollController::class, 'store'])->name('polls.store');
+
+Route::get('/polls/{poll}', [PollController::class, 'show'])->name('polls.show');
+Route::post('/polls/{poll}/vote', [VoteController::class, 'store'])->name('polls.vote');
